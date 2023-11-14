4489b599f1f3d55e62a280ab33b5cacd13ece650
Pavlos Petoumenos <pavlos.petoumenos@manchester.ac.uk>
2023-11-14 11:49:31 +0000

    fix

--- CPP_W3-CPP11/06-constexpr/__init__.py
+++ CPP_W3-CPP11/06-constexpr/__init__.py
@@ -19,7 +19,8 @@ def validate(sources_buf):
 
 @check50.check(compiles)
 def output_correct():
-    output = check50.run("./constexpr").stdout().exit()
+    output = check50.run("./constexpr").stdout()
+    self.exit()
     print(output)
     if not re.match("Array size is: 16378. Program run for \d+ microseconds", output):
         raise check50.Mismatch("Array size is: 16378. Program run for \d+ microseconds", output)
