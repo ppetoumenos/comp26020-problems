41a4328e7e2574913b2dfcdecad902c433125dee
Pavlos Petoumenos <pavlos.petoumenos@manchester.ac.uk>
2023-11-14 12:11:14 +0000

    fix

--- CPP_W3-CPP11/06-constexpr/__init__.py
+++ CPP_W3-CPP11/06-constexpr/__init__.py
@@ -2,7 +2,7 @@ import check50
 import check50.c
 import re
 
-@check50.check()
+@check50.check(timeout=60)
 def exists():
     check50.exists("constexpr.cpp")
     with open("constexpr.cpp") as f:
@@ -19,7 +19,7 @@ def validate(sources_buf):
 
 @check50.check(compiles, timeout=60)
 def output_correct():
-    output = check50.run("./constexpr").stdout(timeout=5)
+    output = check50.run("./constexpr").stdout(timeout=60)
     check50.log(output)
     if not re.match("Array size is: 16378. Program run for \d+ microseconds", output):
         raise check50.Mismatch("Array size is: 16378. Program run for \d+ microseconds", output)
